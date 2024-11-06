1b95ee8f7b91ee179d122f0f34420745efd0caf9
Jonathan <advjavierveas@gmail.com>
2024-11-06 11:16:23 -0500

    commit de prueba

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/products")
+def read_root():
+    return {"Hello": "Aqui escribieremos los productos"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
